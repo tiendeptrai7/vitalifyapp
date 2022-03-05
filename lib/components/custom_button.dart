5c0e33aa9fba4534b08ea258d6835941045e0486
tiendeptrai7 <77587642+tiendeptrai7@users.noreply.github.com>
2022-03-05 10:55:20 +0700

    buoi2
    buoi2

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:vitalifyapp/components/text_styles.dart';
+
+import '../../constants.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+
+  const AuthButton({Key? key, required this.onTap, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.07,
+        margin: const EdgeInsets.only(left: 20, right: 20),
+        decoration: const BoxDecoration(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.all(const Radius.circular(15))),
+        child: Center(
+          child: Text(
+            text,
+            style: KTextStyle.authButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+
+const String primaryColor = "#2A99F2";
+Color priColor = const Color.fromRGBO(40, 155, 243, 10);
+
+const kPrimaryColor = Color(0xFF46494A);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
